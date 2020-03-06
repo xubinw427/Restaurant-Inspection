1f706c5c3f7908406414905c0b7669ab97f77f6b
calvinvinnn <calvinvinnn@users.noreply.github.com>
2020-03-05 20:58:47 -0800

    Add getter setters for violation elements and getter for dictionary

--- app/src/main/java/ca/cmpt276/restaurantinspection/Model/Violation.java
+++ app/src/main/java/ca/cmpt276/restaurantinspection/Model/Violation.java
@@ -4,10 +4,39 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class Violation {
-    public class ViolationElements{
-        String severity;
-        String description;
-        String repeat;
+    public class ViolationElements {
+        private String severity;
+        private String description;
+        private String repeat;
+
+        public String getSeverity() {
+            return severity;
+        }
+
+        public void setSeverity(String severity) {
+            this.severity = severity;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getRepeat() {
+            return repeat;
+        }
+
+        public void setRepeat(String repeat) {
+            this.repeat = repeat;
+        }
     }
+
     Dictionary<Integer, ViolationElements> list = new Hashtable();
+
+    public Dictionary<Integer, ViolationElements> getList() {
+        return list;
+    }
 }
