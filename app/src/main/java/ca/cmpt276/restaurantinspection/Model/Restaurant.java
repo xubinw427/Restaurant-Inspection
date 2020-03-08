076985b2f5372385f2574a7cdd7863da52a64824
Cynthia Zhao <zjzhao@sfu.ca>
2020-03-07 16:12:03 -0800

    Changed initialization of Restaurant object >> takes in string of lumped data and parses information into each class member

---
+++ app/src/main/java/ca/cmpt276/restaurantinspection/Model/Restaurant.java
@@ -0,0 +1,48 @@
+package ca.cmpt276.restaurantinspection.Model;
+
+public class Restaurant {
+    private String ID;
+    private String name;
+    private String address;
+    private String coordinates;
+    private double latitude;
+    private double longitude;
+
+    public Restaurant(String restaurantLump) {
+        String[] restaurantInfo = restaurantLump.split(",");
+        /** [0: ID, 1: Name, 2: PhysAddress, 3: PhysCity, 4: Factype, 5: Latitude, 6: Longitude] **/
+        ID = restaurantInfo[0].replaceAll("\"", "");
+        name = restaurantInfo[1].replaceAll("\"", "");
+        address = restaurantInfo[2].replaceAll("\"", "") + ", " +
+                  restaurantInfo[3].replaceAll("\"", "");
+
+        coordinates = restaurantInfo[5].replaceAll("\"", "") + ", " +
+                restaurantInfo[6].replaceAll("\"", "");
+        latitude = Double.parseDouble(restaurantInfo[5]);
+        longitude = Double.parseDouble(restaurantInfo[6]);
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    private double getLatitude() {
+        return latitude;
+    }
+
+    private double getLongitude() {
+        return longitude;
+    }
+}
