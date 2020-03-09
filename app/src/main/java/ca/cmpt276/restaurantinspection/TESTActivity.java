b4605a2313b871abf1a428fd8af0ab4bf589d56d
Cynthia Zhao <zjzhao@sfu.ca>
2020-03-08 17:23:09 -0700

    Created CardView Layout for main activity >> populated with dummy data >> TODO: Re-point Adapter to Restaurant from TestRestaurant after

---
+++ app/src/main/java/ca/cmpt276/restaurantinspection/TESTActivity.java
@@ -0,0 +1,14 @@
+package ca.cmpt276.restaurantinspection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TESTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
