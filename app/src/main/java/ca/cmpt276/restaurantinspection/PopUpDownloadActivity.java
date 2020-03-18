21a182aba5d5a0f4d904273541dd22b393851844
calvinvinnn <calvinvinnn@users.noreply.github.com>
2020-03-17 18:04:07 -0700

    Add popup window for downloading, add loading animation, remove action bar, and add title

---
+++ app/src/main/java/ca/cmpt276/restaurantinspection/PopUpDownloadActivity.java
@@ -0,0 +1,35 @@
+package ca.cmpt276.restaurantinspection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+public class PopUpDownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_download);
+
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+
+    }
+
+
+
+}
--- app/src/main/java/ca/cmpt276/restaurantinspection/RestaurantMapActivity.java
+++ app/src/main/java/ca/cmpt276/restaurantinspection/RestaurantMapActivity.java
@@ -51,6 +51,7 @@ public class RestaurantMapActivity extends AppCompatActivity implements OnMapRea
         actionBar.setTitle(getString(R.string.title_restaurant_map));
         actionBar.setElevation(0);
 
+
         InputStream restaurantsIn = getResources().openRawResource(R.raw.restaurants_itr1);
         InputStream inspectionsIn = getResources().openRawResource(R.raw.inspectionreports_itr1);
         InputStream violationsIn = getResources().openRawResource(R.raw.all_violations);
